a06d2877a191592b41500e3a579547bfde353527
nicenathapong <nicenathapong@gmail.com>
2022-05-27 18:45:09 +0700

    Update fonts

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+    <body class="min-h-screen bg-gray-800">
+        <Nuxt />
+    </body>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -13,4 +13,9 @@ export default {
     css: [
         '@/assets/css/main.css',
     ],
+    head: {
+        link: [
+            { rel: "stylesheet", href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap' }
+        ]
+    }
 }
